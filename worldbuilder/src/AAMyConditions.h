fa34bc85742d837e0dd2587d73d516f980502ddb
Arun Augustine <arunaugustine87@gmail.com>
2013-04-06 13:04:32 -0500

    adding only new files to adding arDragBehaviors for scaling along X axis
    redoing part of the reverted commit

---
+++ worldbuilder/src/AAMyConditions.h
@@ -0,0 +1,40 @@
+#include "arInteractionUtilities.h"
+
+#include <utility>
+#include <list>
+#include <set>
+
+
+
+class WB_ScaleObjectAlongXAxis : public arDragBehavior {
+public:
+	/*
+	Constructor takes left and right effectors, a scaling factor
+	that determines how much to scale object by and a boolean
+	parameter allowOffset.
+	grabbingEffector is the effector grabbing the object. 
+	changingEffector is the effector that is used to scale object.
+	
+	*/
+	WB_ScaleObjectAlongXAxis(arEffector *grabbingEffector, arEffector *changingEffector, float scalingFactor, bool allowOffset);
+	WB_ScaleObjectAlongXAxis(const WB_ScaleObjectAlongXAxis& c);
+
+	~WB_ScaleObjectAlongXAxis() {} // destructor
+
+	void init(const arEffector* const effector, const arInteractable* const object);
+	void update(const arEffector* const effector, arInteractable* const object,
+		const arGrabCondition* const grabCondition);
+	arDragBehavior* copy() const;
+
+private:
+	arEffector *_grabbingEffector;
+	arEffector *_changingEffector;
+
+	float _scalingFactor;
+
+	bool _allowOffset;
+	arMatrix4 _scaleOffsetMatrix;
+
+	float oldDist;
+	float newDist;
+};
