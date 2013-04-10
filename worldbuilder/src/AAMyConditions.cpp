6319bf0b552f00925783cfbd062bc34bdb1301b5
Arun Augustine <arunaugustine87@gmail.com>
2013-04-10 13:04:58 -0500

    adding WB_ScaleObjectAlongXAxis class
    grab with one effector, change the scale along x axis using the other. a
    scaling factor is to be supplied.

--- worldbuilder/src/AAMyConditions.cpp
+++ worldbuilder/src/AAMyConditions.cpp
@@ -10,3 +10,86 @@
 using namespace std;
 
 
+ WB_ScaleObjectAlongXAxis::WB_ScaleObjectAlongXAxis(arEffector *grabbingEffector, arEffector *changingEffector, float scalingFactor, 
+	bool allowOffset):
+	arDragBehavior(),
+	_grabbingEffector(grabbingEffector),
+	_changingEffector(changingEffector),
+	_scalingFactor(scalingFactor),
+	_allowOffset(allowOffset),
+	_scaleOffsetMatrix(),
+	_oldDist(NaN),
+	_newdist(NaN)
+	{}
+
+WB_ScaleObjectAlongXAxis::WB_ScaleObjectAlongXAxis(const WB_ScaleObjectAlongXAxis& d):
+	arDragBehavior(),
+	_grabbingEffector(d._grabbingEffector),
+	_changingEffector(d._changingEffector),
+	_scalingFactor(d._scalingFactor),
+	_allowOffset(d._allowOffset),
+	_scaleOffsetMatrix(d._scaleOffsetMatrix),
+	_oldDist(d._oldDist),
+	_newdist(d._newDist)
+	{}
+
+void WB_ScaleObjectAlongXAxis::init(const arEffector* const /* effector*/ , const arInteractable* const /*object*/)
+	{
+		arMatrix4 grabbingEffectorMat = _grabbingEffector->getMatrix();
+		float x1 = grabbingEffectorMat.v[12];
+		float y1 = grabbingEffectorMat.v[13];
+		float z1 = grabbingEffectorMat.v[14];
+
+		arMatrix4 changingEffectorMat = _changingEffector->getMatrix();
+		float x2 = changingEffectorMat.v[12];
+		float y2 = changingEffectorMat.v[13];
+		float z2 = changingEffectorMat.v[14];
+
+		// set up the initial distance between the effectors
+		_oldDist = arVector3(x2-x1, y2-y1, z2-z1).magnitude();
+
+		// set up the scaling factor. Currently a fixed value.
+		// TODO we can add a construcutor to use the scaling factor 
+		// derived from the object size and relative distance between wands
+
+	}
+
+void WB_ScaleObjectAlongXAxis::update(const arEffector* const /*effector*/ , arInteractable* const object,
+										const arGrabCondition* const /*grabCondition*/)
+	{
+		arMatrix4 grabbingEffectorMat = _grabbingEffector->getMatrix();
+		float x1 = grabbingEffectorMat.v[12];
+		float y1 = grabbingEffectorMat.v[13];
+		float z1 = grabbingEffectorMat.v[14];
+
+		arMatrix4 changingEffectorMat = _changingEffector->getMatrix();
+		float x2 = changingEffectorMat.v[12];
+		float y2 = changingEffectorMat.v[13];
+		float z2 = changingEffectorMat.v[14];
+
+		// get the current distance between the effectors and set it as the
+		// new distance
+		_newDist = arVector3(x2-x1, y2-y1, z2-z1).magnitude();
+
+		float epsilonChange = 0.005 // minimum change value required to update the matrix
+
+		if (_newDist > _oldDist + epsilonChange || _newdist < _oldDist - epsilonChange)
+		{
+			assert(_oldDist != 0.0) // assert we are not dividing by zero
+
+			float diff = (_newDist / _oldDist) * _scalingFactor; 
+			arMatrix4 objectMatrix = object->getMatrix();
+			object->setMatrix(objectMatrix * ar_SM(diff, 1.0, 1.0));
+		}
+		_oldDist = _newDist;
+	}
+
+void WB_ScaleObjectAlongXAxis::copy() const
+{
+	return (arDragBehavior*) new WB_ScaleObjectAlongXAxis(*this);
+}
+
+
+
+
+
