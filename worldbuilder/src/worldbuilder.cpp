e1b85743f1b560b25fc3d34ac8731fc0ba1ce914
James Hall <jmh045000@utdallas.edu>
2013-04-25 14:31:28 -0500

    add help screen (press both HOME buttons)

--- worldbuilder/src/worldbuilder.cpp
+++ worldbuilder/src/worldbuilder.cpp
@@ -64,6 +64,7 @@ WiiMote primary( WiiMote::CONTROLLER_1 );
 WiiMote secondary( WiiMote::CONTROLLER_2 );
 Ground ground;
 bool menuOn = false;
+bool helpOn = false;
 
 std::list<arInteractable*> interactableObjects;
 
@@ -119,14 +120,20 @@ bool initSceneGraph( arMasterSlaveFramework &fw, arSZGClient& /*Unused*/)
 
 inline void setMenuOn()
 {
-    menuOn = true;
-    buildMenu( menu );
+	if( !menuOn )
+	{
+		menuOn = true;
+		buildMenu( menu );
+	}
 }
 
 inline void setMenuOff()
 {
-    menuOn = false;
-    tearDownMenu( menu );
+	if( menuOn )
+	{
+		menuOn = false;
+		tearDownMenu( menu );
+	}
 }
 
 inline void toggleMenu( )
@@ -239,226 +246,37 @@ void moveWorld( arSZGAppFramework &fw )
     }
 }
 
-void onPreExchange( arMasterSlaveFramework &fw )
+void drawHelp( arMasterSlaveFramework &fw )
 {
-    fw.navUpdate();
-    // update the input state (placement matrix & button states) of our effector.
-    primary.updateState( fw.getInputState() );
-    secondary.updateState( fw.getInputState() );
-
-    //used for scale the world (and possibly other scales later)
-    WiiMote::updateTipDistance(primary, secondary);
-    scaleWorld();
-    moveWorld( fw );
-    
-    std::map<WiiMote::button_t, std::list<WiiMote*> > buttonMap;
-    WiiMote::ButtonList buttons = secondary.getDownButtons();
-    for( WiiMote::ButtonList::iterator it = buttons.begin(); it != buttons.end(); ++it )
-    {  // Process all butons just pressed on secondary
-        if( buttonMap.find( *it ) == buttonMap.end() )
-            buttonMap.insert( std::make_pair( *it, std::list<WiiMote*>( 1, &secondary ) ) );
-        else
-            buttonMap[*it].push_back( &secondary );
-    }
-    
-    buttons = primary.getDownButtons();
-    for( WiiMote::ButtonList::iterator it = buttons.begin(); it != buttons.end(); ++it )
-    {   // Process all butons just pressed on primary
-        if( buttonMap.find( *it ) == buttonMap.end() )
-            buttonMap.insert( std::make_pair( *it, std::list<WiiMote*>( 1, &primary ) ) );
-        else
-            buttonMap[*it].push_back( &primary );
-    }
-    
-    for( std::map<WiiMote::button_t, std::list<WiiMote*> >::iterator it = buttonMap.begin(); it != buttonMap.end(); ++it )
-    {
-        switch( it->first )
-        {
-        case WiiMote::HOME:
-            toggleMenu();
-            break;
-        case WiiMote::DOWN:
-            if( menuOn )
-                menu->pressedDown();
-            break;
-        case WiiMote::RIGHT:
-            if( menuOn )
-                menu->pressedRight();
-            break;
-        case WiiMote::LEFT:
-            if( menuOn )
-                menu->pressedLeft();
-            break;
-        case WiiMote::UP:
-            if( menuOn )
-                menu->pressedUp();
-            break;
-		case WiiMote::MINUS:
-			cout << "MINUS was pressed" << endl;
-            if( !menuOn )
-            {
-                iSelectMenuObj = 1;
-				for( std::list<arInteractable*>::iterator it = SelectedObjects.begin(); it != SelectedObjects.end(); ++it )
-					if( Node *n = dynamic_cast<Node*>( *it ) )
-						n->setSelected( false );
-                SelectedObjects.clear();
-            }
-            break;
-		case WiiMote::PLUS:
-			cout << "PLUS was pressed" << endl;
-            if( !menuOn )
-                iSelectMenuObj = 2;
-            break;
-        case WiiMote::A:
-			iSelectMenuObj = 0;
-            if( menuOn )
-            {
-                switch( menu->pressedA() )
-                {
-                case REDRAW:
-                    setMenuOff();
-                    setMenuOn();
-                    break;
-                case CLOSE:
-                    setMenuOff();
-                    break;
-                default:
-                    break;
-                }
-            }
-            break;
-        case WiiMote::ONE:
-            for(std::list<WiiMote*>::iterator itt = it->second.begin(); itt != it->second.end(); ++itt)
-            {
-                (*itt)->toggleLightSaber();
-            }
-            break;
-        default:
-            break;
-        }
-    }
+	static arTexture tex;
+	static bool init = false;
+	if( !init )
+	{
+		tex.readJPEG( "help.jpg" );
+	}
 	
-    // do ray-casting after menu actions
-    if ( !menuOn ) {
-        rightClosest = primary.closestObject(interactableObjects);
-        leftClosest = secondary.closestObject(interactableObjects);
-    }
-	
-    if( rightClosest )
+	static float size = 4;
+    static GLfloat v[4][3] =
     {
-        rightClosest->touch( primary );
+        { -size / 2, -size / 2, 0.0001 },
+        { size / 2, -size / 2, 0.0001 },
+        { size / 2, size / 2, 0.0001 },
+        { -size / 2, size / 2, 0.0001 }
+    };
+	
+	glPushMatrix();
+		glMultMatrixf( ( ar_getNavMatrix() * fw.getMidEyeMatrix() * ar_TM( 0, 0, -5 ) ).v );
 		
-		if( primary.getButton( WiiMote::A ) && primary.getButton( WiiMote::B ) )
-		{
-			primary.requestScaleGrab( rightClosest );
-		}
-        else if( primary.getButton( WiiMote::A ) )
-        {
-            primary.requestPosGrab( rightClosest );
-        }
-		else if( primary.getButton( WiiMote::B ) )
-		{
-			primary.requestRotGrab( rightClosest );
-		}
-		else if(((primary.getButton( WiiMote::PLUS )) || (primary.getButton( WiiMote::MINUS ))) && ((iSelectMenuObj == 1) || (iSelectMenuObj == 2)))
-		{
-			bool found = false;
-			std::list<arInteractable*> tempSelectedObjects;
-			while (!SelectedObjects.empty())
-			{
-				if ( SelectedObjects.front() == rightClosest )
-				{
-					cout << "Removing right hand object to selected list" << endl;
-					rightClosest->setSelected( false );
-					SelectedObjects.pop_front();
-					found = true;
-					iSelectMenuObj = 0;
-					break;
-				}
-				else
-				{
-					tempSelectedObjects.push_back( SelectedObjects.front() );
-					SelectedObjects.pop_front();
-				}
-			}
-			
-			while (!tempSelectedObjects.empty())
-			{
-				SelectedObjects.push_back( tempSelectedObjects.front() );
-				if( Node *n = dynamic_cast<Node*>( tempSelectedObjects.front() ) )
-					n->setSelected( true );
-				tempSelectedObjects.pop_front();
-			}
-			
-			if (!found)
-			{
-				cout << "Adding right hand object to selected list" << endl;
-				SelectedObjects.push_back( rightClosest );
-				rightClosest->setSelected( true );
-				iSelectMenuObj = 0;
-			}
-		}
-    }
-    else
-        primary.forceUngrab();
-        
-    if( leftClosest )
-    {
-        leftClosest->touch( secondary );
-		if( secondary.getButton( WiiMote::A ) && secondary.getButton( WiiMote::B ) )
-		{
-			secondary.requestScaleGrab( leftClosest );
-		}
-        else if( secondary.getButton( WiiMote::A ) )
-        {
-            secondary.requestPosGrab( leftClosest );
-        }
-		if( secondary.getButton( WiiMote::B ) )
-		{
-			secondary.requestRotGrab( leftClosest );
-		}
-		else if(((secondary.getButton( WiiMote::PLUS )) || (secondary.getButton( WiiMote::MINUS ))) && ((iSelectMenuObj == 1) || (iSelectMenuObj == 2)))
-		{
-			bool found = false;
-			std::list<arInteractable*> tempSelectedObjects;
-			while (!SelectedObjects.empty())
-			{
-				if ( SelectedObjects.front() == leftClosest )
-				{
-					cout << "Removing left hand object to selected list" << endl;
-					leftClosest->setSelected( false );
-					SelectedObjects.pop_front();
-					found = true;
-					iSelectMenuObj = 0;
-					break;
-				}
-				else
-				{
-					tempSelectedObjects.push_back( SelectedObjects.front() );
-					SelectedObjects.pop_front();
-				}
-			}
-			
-			while (!tempSelectedObjects.empty())
-			{
-				SelectedObjects.push_back( tempSelectedObjects.front() );
-				if( Node *n = dynamic_cast<Node*>( tempSelectedObjects.front() ) )
-					n->setSelected( true );
-				tempSelectedObjects.pop_front();
-			}
-			
-			if (!found)
-			{
-				cout << "Adding left hand object to selected list" << endl;
-				SelectedObjects.push_back( leftClosest );
-				leftClosest->setSelected( true );
-				iSelectMenuObj = 0;
-			}
-		}
-    }
-    else
-        secondary.forceUngrab();
-	
+		glClearColor( 1, 1, 1, 0 );
+		tex.activate();
+		glBegin( GL_QUADS );
+			glTexCoord2f( 0, 0 ); glVertex3fv( v[0] );
+			glTexCoord2f( 1, 0 ); glVertex3fv( v[1] );
+			glTexCoord2f( 1, 1 ); glVertex3fv( v[2] );
+			glTexCoord2f( 0, 1 ); glVertex3fv( v[3] );
+		glEnd();
+		tex.deactivate();
+	glPopMatrix();
 }
 
 void doSceneGraph( arMasterSlaveFramework &fw )
@@ -488,6 +306,250 @@ void doSceneGraph( arMasterSlaveFramework &fw )
         loops = 0;
     }
 }
+
+void onPreExchange( arMasterSlaveFramework &fw )
+{
+    fw.navUpdate();
+    // update the input state (placement matrix & button states) of our effector.
+    primary.updateState( fw.getInputState() );
+    secondary.updateState( fw.getInputState() );
+	
+	if( primary.getButton( WiiMote::HOME ) && secondary.getButton( WiiMote::HOME ) )
+	{
+		helpOn = true;
+		setMenuOff();
+		fw.setDrawCallback( drawHelp );
+	}
+	
+	if( helpOn )
+	{
+		if( primary.getButton( WiiMote::A ) || secondary.getButton( WiiMote::A ) )
+		{
+			helpOn = false;
+			fw.setDrawCallback( doSceneGraph );
+			return;
+		}
+	}
+	else
+	{
+
+		//used for scale the world (and possibly other scales later)
+		WiiMote::updateTipDistance(primary, secondary);
+		scaleWorld();
+		moveWorld( fw );
+		
+		std::map<WiiMote::button_t, std::list<WiiMote*> > buttonMap;
+		WiiMote::ButtonList buttons = secondary.getDownButtons();
+		for( WiiMote::ButtonList::iterator it = buttons.begin(); it != buttons.end(); ++it )
+		{  // Process all butons just pressed on secondary
+			if( buttonMap.find( *it ) == buttonMap.end() )
+				buttonMap.insert( std::make_pair( *it, std::list<WiiMote*>( 1, &secondary ) ) );
+			else
+				buttonMap[*it].push_back( &secondary );
+		}
+		
+		buttons = primary.getDownButtons();
+		for( WiiMote::ButtonList::iterator it = buttons.begin(); it != buttons.end(); ++it )
+		{   // Process all butons just pressed on primary
+			if( buttonMap.find( *it ) == buttonMap.end() )
+				buttonMap.insert( std::make_pair( *it, std::list<WiiMote*>( 1, &primary ) ) );
+			else
+				buttonMap[*it].push_back( &primary );
+		}
+		
+		for( std::map<WiiMote::button_t, std::list<WiiMote*> >::iterator it = buttonMap.begin(); it != buttonMap.end(); ++it )
+		{
+			switch( it->first )
+			{
+			case WiiMote::HOME:
+				toggleMenu();
+				break;
+			case WiiMote::DOWN:
+				if( menuOn )
+					menu->pressedDown();
+				break;
+			case WiiMote::RIGHT:
+				if( menuOn )
+					menu->pressedRight();
+				break;
+			case WiiMote::LEFT:
+				if( menuOn )
+					menu->pressedLeft();
+				break;
+			case WiiMote::UP:
+				if( menuOn )
+					menu->pressedUp();
+				break;
+			case WiiMote::MINUS:
+				cout << "MINUS was pressed" << endl;
+				if( !menuOn )
+				{
+					iSelectMenuObj = 1;
+					for( std::list<arInteractable*>::iterator it = SelectedObjects.begin(); it != SelectedObjects.end(); ++it )
+						if( Node *n = dynamic_cast<Node*>( *it ) )
+							n->setSelected( false );
+					SelectedObjects.clear();
+				}
+				break;
+			case WiiMote::PLUS:
+				cout << "PLUS was pressed" << endl;
+				if( !menuOn )
+					iSelectMenuObj = 2;
+				break;
+			case WiiMote::A:
+				iSelectMenuObj = 0;
+				if( menuOn )
+				{
+					switch( menu->pressedA() )
+					{
+					case REDRAW:
+						setMenuOff();
+						setMenuOn();
+						break;
+					case CLOSE:
+						setMenuOff();
+						break;
+					default:
+						break;
+					}
+				}
+				break;
+			case WiiMote::ONE:
+				for(std::list<WiiMote*>::iterator itt = it->second.begin(); itt != it->second.end(); ++itt)
+				{
+					(*itt)->toggleLightSaber();
+				}
+				break;
+			default:
+				break;
+			}
+		}
+		
+		// do ray-casting after menu actions
+		if ( !menuOn ) {
+			rightClosest = primary.closestObject(interactableObjects);
+			leftClosest = secondary.closestObject(interactableObjects);
+		}
+		
+		if( rightClosest )
+		{
+			rightClosest->touch( primary );
+			
+			if( primary.getButton( WiiMote::A ) && primary.getButton( WiiMote::B ) )
+			{
+				primary.requestScaleGrab( rightClosest );
+			}
+			else if( primary.getButton( WiiMote::A ) )
+			{
+				primary.requestPosGrab( rightClosest );
+			}
+			else if( primary.getButton( WiiMote::B ) )
+			{
+				primary.requestRotGrab( rightClosest );
+			}
+			else if(((primary.getButton( WiiMote::PLUS )) || (primary.getButton( WiiMote::MINUS ))) && ((iSelectMenuObj == 1) || (iSelectMenuObj == 2)))
+			{
+				bool found = false;
+				std::list<arInteractable*> tempSelectedObjects;
+				while (!SelectedObjects.empty())
+				{
+					if ( SelectedObjects.front() == rightClosest )
+					{
+						cout << "Removing right hand object to selected list" << endl;
+						rightClosest->setSelected( false );
+						SelectedObjects.pop_front();
+						found = true;
+						iSelectMenuObj = 0;
+						break;
+					}
+					else
+					{
+						tempSelectedObjects.push_back( SelectedObjects.front() );
+						SelectedObjects.pop_front();
+					}
+				}
+				
+				while (!tempSelectedObjects.empty())
+				{
+					SelectedObjects.push_back( tempSelectedObjects.front() );
+					if( Node *n = dynamic_cast<Node*>( tempSelectedObjects.front() ) )
+						n->setSelected( true );
+					tempSelectedObjects.pop_front();
+				}
+				
+				if (!found)
+				{
+					cout << "Adding right hand object to selected list" << endl;
+					SelectedObjects.push_back( rightClosest );
+					rightClosest->setSelected( true );
+					iSelectMenuObj = 0;
+				}
+			}
+		}
+		else
+			primary.forceUngrab();
+			
+		if( leftClosest )
+		{
+			leftClosest->touch( secondary );
+			if( secondary.getButton( WiiMote::A ) && secondary.getButton( WiiMote::B ) )
+			{
+				secondary.requestScaleGrab( leftClosest );
+			}
+			else if( secondary.getButton( WiiMote::A ) )
+			{
+				secondary.requestPosGrab( leftClosest );
+			}
+			if( secondary.getButton( WiiMote::B ) )
+			{
+				secondary.requestRotGrab( leftClosest );
+			}
+			else if(((secondary.getButton( WiiMote::PLUS )) || (secondary.getButton( WiiMote::MINUS ))) && ((iSelectMenuObj == 1) || (iSelectMenuObj == 2)))
+			{
+				bool found = false;
+				std::list<arInteractable*> tempSelectedObjects;
+				while (!SelectedObjects.empty())
+				{
+					if ( SelectedObjects.front() == leftClosest )
+					{
+						cout << "Removing left hand object to selected list" << endl;
+						leftClosest->setSelected( false );
+						SelectedObjects.pop_front();
+						found = true;
+						iSelectMenuObj = 0;
+						break;
+					}
+					else
+					{
+						tempSelectedObjects.push_back( SelectedObjects.front() );
+						SelectedObjects.pop_front();
+					}
+				}
+				
+				while (!tempSelectedObjects.empty())
+				{
+					SelectedObjects.push_back( tempSelectedObjects.front() );
+					if( Node *n = dynamic_cast<Node*>( tempSelectedObjects.front() ) )
+						n->setSelected( true );
+					tempSelectedObjects.pop_front();
+				}
+				
+				if (!found)
+				{
+					cout << "Adding left hand object to selected list" << endl;
+					SelectedObjects.push_back( leftClosest );
+					leftClosest->setSelected( true );
+					iSelectMenuObj = 0;
+				}
+			}
+		}
+		else
+			secondary.forceUngrab();
+	}
+	
+}
+
+
 
 
 int main(int argc, char *argv[])
